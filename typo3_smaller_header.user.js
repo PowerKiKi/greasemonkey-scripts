b59a045c2614430ab6a2361ec1c65b99589ec4de
PowerKiKi <adrien.crivelli@gmail.com>
2010-06-04 15:40:56 +0200

    ADD script for forge.typo3.org

---
+++ typo3_smaller_header.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name           typo3 smaller header
+// @namespace      typo3
+// @include        http://forge.typo3.org/*extension-newsletter*
+// ==/UserScript==
+
+GM_addStyle('#header { height: 100px; }'); 
+GM_addStyle('#header>#right-area>h1 { padding: 0 0 0 0; }'); 
+GM_addStyle('#header-left { padding: 0 0 0 0; }'); 
+GM_addStyle('#header>#header-left>#header-menu { padding: 0 0 0 0; font-size: 5pt; }'); 
+GM_addStyle('#header>#header-left>#header-menu>a.start  {margin: 0 0 0 0 }');
