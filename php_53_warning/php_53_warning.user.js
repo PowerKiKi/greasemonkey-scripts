7d8c6ab0dd2f39e2e2714cba3d413f2f39e6b5ca
PowerKiKi <adrien.crivelli@gmail.com>
2011-02-23 14:05:24 +0100

    worked only for www.php.net

--- php_53_warning/php_53_warning.user.js
+++ php_53_warning/php_53_warning.user.js
@@ -2,7 +2,7 @@
 // @name           PHP 5.3 warning
 // @namespace      PHP
 // @description    Display a red background to warn when function exists from PHP 5.3 only
-// @include        http://www.php.net/manual/*
+// @include        *php.net/manual/*
 // ==/UserScript==
 
 if (document.evaluate('count(//p[contains(text(), "5.3.0")])', document, null, XPathResult.NUMBER_TYPE, null).numberValue > 0)
